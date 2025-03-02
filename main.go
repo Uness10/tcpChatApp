af6bc8ed8049030ac2e4ba12d101e503077e0b90
Uness10 <younessanouar57@gmail.com>
2025-03-02 01:20:43 +0000

    first

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"net"
+)
+
+func main() {
+	s := newServer()
+	go s.run()
+
+	listener, err := net.Listen("tcp", "0.0.0.0:8888")
+
+	if err != nil {
+		log.Fatalf("unable to start server: %s", err.Error())
+	}
+	defer listener.Close()
+	log.Println("Started server on :8888")
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Printf("Unable to accept connection :%s", err.Error())
+			continue
+		}
+		go s.newClient(conn)
+
+	}
+}
